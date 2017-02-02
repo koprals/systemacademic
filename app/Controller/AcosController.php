cc0096143dfbbadb7a4b929ca342921dd25d742e
koprals <kopralrhp@gmail.com>
2017-02-02 23:52:15 +0700

    settings dikit

--- app/Controller/AcosController.php
+++ app/Controller/AcosController.php
@@ -6,14 +6,14 @@ class AcosController extends AppController
 	var $helpers			=	array("Text","Aimfox");
 	var $uses				=	"MyAco";
 	var $aco_id;
-	
+
 	function beforeFilter()
 	{
 		parent::beforeFilter();
 		$this->set("ControllerName",$this->ControllerName);
 		$this->set("ModelName",$this->ModelName);
 		$this->set('lft_menu_category_id',"3");
-		
+
 		//CHECK PRIVILEGES
 		$this->loadModel("MyAco");
 		$find					=	$this->MyAco->find("first",array(
@@ -24,7 +24,7 @@ class AcosController extends AppController
 		$this->aco_id			=	$find["MyAco"]["id"];
 		$this->set("aco_id",$this->aco_id);
 	}
-	
+
 	function Index($page=1,$viewpage=50)
 	{
 		if($this->access[$this->aco_id]["_read"] != "1")
@@ -39,31 +39,31 @@ class AcosController extends AppController
 		$this->Session->delete('Search.'.$this->ControllerName.'Page');
 		$this->Session->delete('Search.'.$this->ControllerName.'Conditions');
 		$this->Session->delete('Search.'.$this->ControllerName.'parent_id');
-		
+
 		//DEFINE NEWS CATEGORY
 		$parent_id_list	=	$this->{$this->ModelName}->generateTreeList();
 		$this->set(compact("page","viewpage","parent_id_list"));
 	}
-	
+
 	function ListItem()
 	{
 		$this->layout	=	"ajax";
-		
+
 		if($this->access[$this->aco_id]["_read"] != "1")
 		{
 			$data			=	array();
 			$this->set(compact("data"));
 			return;
 		}
-		
+
 		$this->loadModel($this->ModelName);
 		$this->{$this->ModelName}->BindDefault(false);
 		$this->{$this->ModelName}->VirtualFieldActivated();
-		
+
 		//GET TOP ID
 		if(!empty($this->params['named']['parent_id']))
 			$this->Session->write('Search.'.$this->ControllerName.'parent_id',$this->params['named']['parent_id']);
-			
+
 		$parent_id				=	$this->Session->read('Search.'.$this->ControllerName.'parent_id');
 		if(is_null($parent_id))
 		{
@@ -79,51 +79,51 @@ class AcosController extends AppController
 		{
 			$parent_id			=	$this->Session->read('Search.'.$this->ControllerName.'parent_id');
 		}
-		
+
 		$check					=	$this->{$this->ModelName}->find("first",array(
 										"conditions"	=>	array(
 											"{$this->ModelName}.id"	=>	$parent_id
 										)
 									));
-		
-								
+
+
 		//DEFINE LAYOUT, LIMIT AND OPERAND
 		$viewpage			=	empty($this->params['named']['limit']) ? 50 : $this->params['named']['limit'];
 		$order				=	array("{$this->ModelName}.alias" => "ASC");
 		$operand			=	"AND";
-		
+
 		//DEFINE SEARCH DATA
 		if(!empty($this->request->data))
 		{
 			$cond_search	=	array();
 			$operand		=	$this->request->data[$this->ModelName]['operator'];
 			$this->Session->delete('Search.'.$this->ControllerName);
-			
+
 			if(!empty($this->request->data['Search']['id']))
 			{
 				$cond_search["{$this->ModelName}.id"]					=	$this->data['Search']['id'];
 			}
-			
+
 			if(!empty($this->request->data['Search']['name']))
 			{
 				$cond_search["{$this->ModelName}.name LIKE "]			=	"%".$this->data['Search']['name']."%";
 			}
-			
+
 			if(!empty($this->request->data['Search']['parent_id']))
 			{
 				$cond_search["{$this->ModelName}.parent_id"]			=	$this->data['Search']['parent_id'];
 			}
-			
+
 			if($this->request->data["Search"]['reset']=="0")
 			{
 				$this->Session->write("Search.".$this->ControllerName,$cond_search);
 				$this->Session->write('Search.'.$this->ControllerName.'Operand',$operand);
 			}
 		}
-		
+
 		$this->Session->write('Search.'.$this->ControllerName.'Viewpage',$viewpage);
 		$this->Session->write('Search.'.$this->ControllerName.'Sort',(empty($this->params['named']['sort']) or !isset($this->params['named']['sort'])) ? $order : $this->params['named']['sort']." ".$this->params['named']['direction']);
-		
+
 		$cond_search		=	array();
 		$filter_paginate	=	array(
 									"{$this->ModelName}.parent_id"	=>	$parent_id
@@ -134,16 +134,16 @@ class AcosController extends AppController
 										'limit'				=>	$viewpage
 									)
 								);
-		
+
 		$ses_cond			=	$this->Session->read("Search.".$this->ControllerName);
 		$cond_search		=	isset($ses_cond) ? $ses_cond : array();
 		$ses_operand		=	$this->Session->read("Search.".$this->ControllerName."Operand");
 		$operand			=	isset($ses_operand) ? $ses_operand : "AND";
 		$merge_cond			=	empty($cond_search) ? $filter_paginate : array_merge($filter_paginate,array($operand => $cond_search) );
 		$data				=	$this->paginate("{$this->ModelName}",$merge_cond);
-		
+
 		$this->Session->write('Search.'.$this->ControllerName.'Conditions',$merge_cond);
-		
+
 		if(isset($this->params['named']['page']) && $this->params['named']['page'] > $this->params['paging'][$this->ModelName]['pageCount'])
 		{
 			$this->params['named']['page']	=	$this->params['paging'][$this->ModelName]['pageCount'];
@@ -152,8 +152,8 @@ class AcosController extends AppController
 		$this->Session->write('Search.'.$this->ControllerName.'Page',$page);
 		$this->set(compact('data','page','viewpage','check',"parent_id"));
 	}
-	
-	
+
+
 	function Excel()
 	{
 		if($this->access[$this->aco_id]["_read"] != "1")
@@ -161,23 +161,23 @@ class AcosController extends AppController
 			$this->layout	=	"no_access";
 			return;
 		}
-		
+
 		if(isset($_GET['debug']) && $_GET['debug'] == "1")
 			$this->layout		=	"ajax";
 		else
 			$this->layout		=	"xls";
-		
+
 		$this->response->type(array('xls' => 'application/vnd.ms-excel'));
     	$this->response->type('xls');
-	
+
 		$this->{$this->ModelName}->BindDefault(false);
 		$this->{$this->ModelName}->VirtualFieldActivated();
-		
+
 		$order				=	$this->Session->read("Search.".$this->ControllerName."Sort");
 		$viewpage			=	$this->Session->read("Search.".$this->ControllerName."Viewpage");
 		$page				=	$this->Session->read("Search.".$this->ControllerName."Page");
 		$conditions			=	$this->Session->read("Search.".$this->ControllerName."Conditions");
-		
+
 		$this->paginate		=	array(
 									"{$this->ModelName}"	=>	array(
 										"order"				=>	$order,
@@ -191,7 +191,7 @@ class AcosController extends AppController
 		$filename			=	$this->ModelName."_".date("dMY");
 		$this->set(compact("data","title","page","viewpage","filename"));
 	}
-	
+
 	function Add()
 	{
 		if($this->access[$this->aco_id]["_create"] != "1")
@@ -199,10 +199,10 @@ class AcosController extends AppController
 			$this->layout	=	"no_access";
 			return;
 		}
-		
+
 		//DEFINE NEWS CATEGORY
 		$parent_id_list	=	$this->{$this->ModelName}->generateTreeList(null,"{n}.{$this->ModelName}.id","{n}.{$this->ModelName}.alias");
-		
+
 		if(!empty($this->request->data))
 		{
 			$this->{$this->ModelName}->set($this->request->data);
@@ -214,10 +214,10 @@ class AcosController extends AppController
 				$this->redirect(array("action"=>"SuccessAdd",$ID));
 			}//END IF VALIDATE
 		}//END IF NOT EMPTY
-		
+
 		$this->set(compact("parent_id_list"));
 	}
-	
+
 	function Edit($ID=NULL,$page=1,$viewpage=50)
 	{
 		if($this->access[$this->aco_id]["_update"] != "1")
@@ -225,10 +225,10 @@ class AcosController extends AppController
 			$this->layout	=	"no_access";
 			return;
 		}
-		
+
 		$this->loadModel($this->ModelName);
 		$this->{$this->ModelName}->VirtualFieldActivated();
-		
+
 		//DEFINE NEWS CATEGORY
 		$parent_id_list	=	$this->{$this->ModelName}->generateTreeList(null,"{n}.{$this->ModelName}.id","{n}.{$this->ModelName}.alias");
 		$detail 			=	$this->{$this->ModelName}->find('first', array(
@@ -236,13 +236,13 @@ class AcosController extends AppController
 										"{$this->ModelName}.id"		=>	$ID
 									)
 								));
-								
+
 		if(empty($detail))
 		{
 			$this->layout	=	"error";
 			return;
 		}
-				
+
 		if (empty($this->data))
 		{
 			$this->data = $detail;
@@ -251,7 +251,7 @@ class AcosController extends AppController
 		{
 			$this->{$this->ModelName}->set($this->data);
 			$this->{$this->ModelName}->ValidateEdit();
-			
+
 			if($this->{$this->ModelName}->validates())
 			{
 				$save		=	$this->{$this->ModelName}->save($this->data,false);
@@ -260,7 +260,7 @@ class AcosController extends AppController
 		}
 		$this->set(compact("ID","detail","parent_id_list","page","viewpage"));
 	}
-	
+
 	function View($ID=NULL)
 	{
 		if($this->access[$this->aco_id]["_read"] != "1")
@@ -268,11 +268,11 @@ class AcosController extends AppController
 			$this->layout	=	"no_access";
 			return;
 		}
-		
+
 		$this->loadModel($this->ModelName);
 		$this->{$this->ModelName}->BindImageBig(false);
 		$this->{$this->ModelName}->VirtualFieldActivated();
-		
+
 		$detail = $this->{$this->ControllerName}->find('first', array(
 			'conditions' => array(
 				"{$this->ControllerName}.id"		=>	$ID
@@ -287,7 +287,7 @@ class AcosController extends AppController
 		}
 		$this->set(compact("ID","detail"));
 	}
-	
+
 	function ChangeStatus($ID=NULL,$status)
 	{
 		if($this->access[$this->aco_id]["_update"] != "1")
@@ -296,13 +296,13 @@ class AcosController extends AppController
 			$this->autoRender	=	false;
 			return;
 		}
-		
+
 		$detail = $this->{$this->ModelName}->find('first', array(
 			'conditions' => array(
 				"{$this->ModelName}.id"		=>	$ID
 			)
 		));
-		
+
 		if(empty($detail))
 		{
 			$message	=	"Item not found.";
@@ -314,11 +314,11 @@ class AcosController extends AppController
 			$this->{$this->ModelName}->save($data);
 			$message	=	"Data has updated.";
 		}
-		
+
 		echo json_encode(array("data"=>array("message"=>$message)));
 		$this->autoRender	=	false;
 	}
-	
+
 	function Delete($ID=NULL)
 	{
 		if($this->access[$this->aco_id]["_delete"] != "1")
@@ -327,13 +327,13 @@ class AcosController extends AppController
 			$this->autoRender	=	false;
 			return;
 		}
-		
+
 		$detail = $this->{$this->ModelName}->find('first', array(
 			'conditions' => array(
 				"{$this->ModelName}.id"		=>	$ID
 			)
 		));
-		
+
 		if(empty($detail))
 		{
 			$message	=	"Item not found.";
@@ -344,15 +344,15 @@ class AcosController extends AppController
 			$this->AroAco->deleteAll(array(
 				"AroAco.aco_id"	=>	$ID
 			));
-			
+
 			$this->{$this->ModelName}->removeFromTree($ID,true);
 			$message	=	"Data has deleted.";
 		}
-		
+
 		echo json_encode(array("data"=>array("message"=>$message)));
 		$this->autoRender	=	false;
 	}
-	
+
 	function SuccessAdd($ID=NULL)
 	{
 		$data = $this->{$this->ModelName}->find('first', array(
@@ -367,7 +367,7 @@ class AcosController extends AppController
 		}
 		$this->set(compact("ID"));
 	}
-	
+
 	function SuccessEdit($ID=NULL,$page=1,$viewpage=50)
 	{
 		$data = $this->{$this->ModelName}->find('first', array(
@@ -375,7 +375,7 @@ class AcosController extends AppController
 				"{$this->ModelName}.id" 		=> $ID
 			)
 		));
-		
+
 		if(empty($data))
 		{
 			$this->layout	=	"ajax";
--- app/Controller/ArosController.php
+++ app/Controller/ArosController.php
@@ -5,14 +5,14 @@ class ArosController extends AppController
 	var $ModelName			=	"MyAro";
 	var $helpers			=	array("Text","Aimfox");
 	var $uses				=	"MyAro";
-	
+
 	function beforeFilter()
 	{
 		parent::beforeFilter();
 		$this->set("ControllerName",$this->ControllerName);
 		$this->set("ModelName",$this->ModelName);
 		$this->set('lft_menu_category_id',"3");
-		
+
 		//CHECK PRIVILEGES
 		$this->loadModel("MyAco");
 		$find					=	$this->MyAco->find("first",array(
@@ -23,7 +23,7 @@ class ArosController extends AppController
 		$this->aco_id			=	$find["MyAco"]["id"];
 		$this->set("aco_id",$this->aco_id);
 	}
-	
+
 	function Index($page=1,$viewpage=50)
 	{
 		if($this->access[$this->aco_id]["_read"] != "1")
@@ -31,7 +31,7 @@ class ArosController extends AppController
 			$this->layout	=	"no_access";
 			return;
 		}
-		
+
 		$this->Session->delete("Search.".$this->ControllerName);
 		$this->Session->delete('Search.'.$this->ControllerName.'Operand');
 		$this->Session->delete('Search.'.$this->ControllerName.'ViewPage');
@@ -39,13 +39,13 @@ class ArosController extends AppController
 		$this->Session->delete('Search.'.$this->ControllerName.'Page');
 		$this->Session->delete('Search.'.$this->ControllerName.'Conditions');
 		$this->Session->delete('Search.'.$this->ControllerName.'parent_id');
-		
+
 		//DEFINE NEWS CATEGORY
 		$this->loadModel($this->ModelName);
 		$parent_id_list	=	$this->{$this->ModelName}->generateTreeList();
 		$this->set(compact("page","viewpage","parent_id_list"));
 	}
-	
+
 	function ListItem()
 	{
 		$this->layout	=	"ajax";
@@ -58,11 +58,11 @@ class ArosController extends AppController
 		$this->loadModel($this->ModelName);
 		$this->{$this->ModelName}->BindDefault(false);
 		$this->{$this->ModelName}->VirtualFieldActivated();
-		
+
 		//GET TOP ID
 		if(!empty($this->params['named']['parent_id']))
 			$this->Session->write('Search.'.$this->ControllerName.'parent_id',$this->params['named']['parent_id']);
-			
+
 		$parent_id				=	$this->Session->read('Search.'.$this->ControllerName.'parent_id');
 		if(is_null($parent_id))
 		{
@@ -78,14 +78,14 @@ class ArosController extends AppController
 		{
 			$parent_id			=	$this->Session->read('Search.'.$this->ControllerName.'parent_id');
 		}
-		
+
 		$check					=	$this->{$this->ModelName}->find("first",array(
 										"conditions"	=>	array(
 											"{$this->ModelName}.id"	=>	$parent_id
 										)
 									));
-		
-								
+
+
 		//DEFINE LAYOUT, LIMIT AND OPERAND AND PAGE
 		$viewpage			=	empty($this->params['named']['limit']) ? 50 : $this->params['named']['limit'];
 		$order				=	array("{$this->ModelName}.lft" => "ASC");
@@ -95,42 +95,42 @@ class ArosController extends AppController
 			$this->params['named']['page']	=	$this->params['paging'][$this->ModelName]['pageCount'];
 		}
 		$page				=	empty($this->params['named']['page']) ? 1 : $this->params['named']['page'];
-		
+
 		//DEFINE SEARCH DATA
 		if(!empty($this->request->data))
 		{
 			$cond_search	=	array();
 			$operand		=	$this->request->data[$this->ModelName]['operator'];
 			$this->Session->delete('Search.'.$this->ControllerName);
-			
+
 			if(!empty($this->request->data['Search']['id']))
 			{
 				$cond_search["{$this->ModelName}.id"]					=	$this->data['Search']['id'];
 			}
-			
+
 			if(!empty($this->request->data['Search']['name']))
 			{
 				$cond_search["{$this->ModelName}.name LIKE "]			=	"%".$this->data['Search']['name']."%";
 			}
-			
+
 			if(!empty($this->request->data['Search']['parent_id']))
 			{
 				$cond_search["{$this->ModelName}.parent_id"]			=	$this->data['Search']['parent_id'];
 			}
-			
+
 			if($this->request->data["Search"]['reset']=="0")
 			{
 				$this->Session->write("Search.".$this->ControllerName,$cond_search);
 				$this->Session->write('Search.'.$this->ControllerName.'Operand',$operand);
 			}
 		}
-		
+
 		$this->Session->write('Search.'.$this->ControllerName.'Viewpage',$viewpage);
 		$this->Session->write('Search.'.$this->ControllerName.'Sort',(empty($this->params['named']['sort']) or !isset($this->params['named']['sort'])) ? $order : $this->params['named']['sort']." ".$this->params['named']['direction']);
-		
+
 		$cond_search			=	array();
 		$filter_paginate		=	array();
-		
+
 		if($this->super_admin_id != $this->profile["Admin"]["id"])
 		{
 			$filter_paginate	=	array(
@@ -143,21 +143,21 @@ class ArosController extends AppController
 											'limit'				=>	$viewpage
 										)
 									);
-		
+
 		$ses_cond				=	$this->Session->read("Search.".$this->ControllerName);
 		$cond_search			=	isset($ses_cond) ? $ses_cond : array();
 		$ses_operand			=	$this->Session->read("Search.".$this->ControllerName."Operand");
 		$operand				=	isset($ses_operand) ? $ses_operand : "AND";
 		$merge_cond				=	empty($cond_search) ? $filter_paginate : array_merge($filter_paginate,array($operand => $cond_search) );
-		
+
 		$data					=	$this->paginate("{$this->ModelName}",$merge_cond);
-		
+
 		$this->Session->write('Search.'.$this->ControllerName.'Conditions',$merge_cond);
 		$this->Session->write('Search.'.$this->ControllerName.'Page',$page);
 		$this->set(compact('data','page','viewpage','check',"parent_id"));
 	}
-	
-	
+
+
 	function Excel()
 	{
 		if($this->access[$this->aco_id]["_read"] != "1")
@@ -165,7 +165,7 @@ class ArosController extends AppController
 			$this->layout	=	"no_access";
 			return;
 		}
-		
+
 		if(isset($_GET['debug']) && $_GET['debug'] == "1")
 			$this->layout		=	"ajax";
 		else
@@ -174,15 +174,15 @@ class ArosController extends AppController
 			$this->response->type(array('xls' => 'application/vnd.ms-excel'));
 			$this->response->type('xls');
 		}
-	
+
 		$this->{$this->ModelName}->BindDefault(false);
 		$this->{$this->ModelName}->VirtualFieldActivated();
-		
+
 		$order				=	$this->Session->read("Search.".$this->ControllerName."Sort");
 		$viewpage			=	$this->Session->read("Search.".$this->ControllerName."Viewpage");
 		$page				=	$this->Session->read("Search.".$this->ControllerName."Page");
 		$conditions			=	$this->Session->read("Search.".$this->ControllerName."Conditions");
-		
+
 		$this->paginate		=	array(
 									"{$this->ModelName}"	=>	array(
 										"order"				=>	$order,
@@ -196,7 +196,7 @@ class ArosController extends AppController
 		$filename			=	$this->ModelName."_".date("dMY");
 		$this->set(compact("data","title","page","viewpage","filename"));
 	}
-	
+
 	function Add()
 	{
 		if($this->access[$this->aco_id]["_create"] != "1")
@@ -204,15 +204,15 @@ class ArosController extends AppController
 			$this->layout	=	"no_access";
 			return;
 		}
-		
+
 		//DEFINE NEWS CATEGORY
 		$parent_id_list	=	$this->{$this->ModelName}->generateTreeList(null,"{n}.{$this->ModelName}.id","{n}.{$this->ModelName}.alias");
-		
+
 		if(!empty($this->request->data))
 		{
 			$this->request->data[$this->ModelName]["parent_id"]	=	"1";
 			$this->request->data[$this->ModelName]["alias"]		=	str_replace(" ","_",$this->request->data[$this->ModelName]["alias_name"]);
-			
+
 			$this->{$this->ModelName}->set($this->request->data);
 			$this->{$this->ModelName}->ValidateAdd();
 			if($this->{$this->ModelName}->validates())
@@ -222,10 +222,10 @@ class ArosController extends AppController
 				$this->redirect(array("action"=>"SuccessAdd",$ID));
 			}//END IF VALIDATE
 		}//END IF NOT EMPTY
-		
+
 		$this->set(compact("parent_id_list"));
 	}
-	
+
 	function Edit($ID=NULL,$page=1,$viewpage=50)
 	{
 		if($this->access[$this->aco_id]["_update"] != "1")
@@ -233,25 +233,25 @@ class ArosController extends AppController
 			$this->layout	=	"no_access";
 			return;
 		}
-		
+
 		$this->loadModel($this->ModelName);
 		$this->{$this->ModelName}->VirtualFieldActivated();
-		
+
 		//DEFINE NEWS CATEGORY
 		$parent_id_list	=	$this->{$this->ModelName}->generateTreeList(null,"{n}.{$this->ModelName}.id","{n}.{$this->ModelName}.alias");
-		
+
 		$detail 			=	$this->{$this->ModelName}->find('first', array(
 									'conditions' => array(
 										"{$this->ModelName}.id"		=>	$ID
 									)
 								));
-								
+
 		if(empty($detail))
 		{
 			$this->layout	=	"error";
 			return;
 		}
-				
+
 		if (empty($this->data))
 		{
 			$this->data = $detail;
@@ -259,10 +259,10 @@ class ArosController extends AppController
 		else
 		{
 			$this->request->data[$this->ModelName]["alias"]	=	str_replace(" ","_",$this->request->data[$this->ModelName]["alias_name"]);
-			
+
 			$this->{$this->ModelName}->set($this->request->data);
 			$this->{$this->ModelName}->ValidateEdit();
-			
+
 			if($this->{$this->ModelName}->validates())
 			{
 				$save		=	$this->{$this->ModelName}->save($this->request->data,false);
@@ -271,7 +271,7 @@ class ArosController extends AppController
 		}
 		$this->set(compact("ID","detail","parent_id_list","page","viewpage"));
 	}
-	
+
 	function View($ID=NULL)
 	{
 		if($this->access[$this->aco_id]["_read"] != "1")
@@ -279,17 +279,17 @@ class ArosController extends AppController
 			$this->layout	=	"no_access";
 			return;
 		}
-		
+
 		$this->loadModel($this->ModelName);
 		$this->{$this->ModelName}->BindImageBig(false);
 		$this->{$this->ModelName}->VirtualFieldActivated();
-		
+
 		$detail = $this->{$this->ModelName}->find('first', array(
 			'conditions' => array(
 				"{$this->ModelName}.id"		=>	$ID
 			)
 		));
-		
+
 		if(empty($detail))
 		{
 			$this->layout	=	"ajax";
@@ -299,7 +299,7 @@ class ArosController extends AppController
 		}
 		$this->set(compact("ID","detail"));
 	}
-	
+
 	function ChangeStatus($ID=NULL,$status)
 	{
 		if($this->access[$this->aco_id]["_update"] != "1")
@@ -308,13 +308,13 @@ class ArosController extends AppController
 			$this->autoRender	=	false;
 			return;
 		}
-		
+
 		$detail = $this->{$this->ModelName}->find('first', array(
 			'conditions' => array(
 				"{$this->ModelName}.id"		=>	$ID
 			)
 		));
-		
+
 		if(empty($detail))
 		{
 			$message	=	"Item not found.";
@@ -326,11 +326,11 @@ class ArosController extends AppController
 			$this->{$this->ModelName}->save($data);
 			$message	=	"Data has updated.";
 		}
-		
+
 		echo json_encode(array("data"=>array("message"=>$message)));
 		$this->autoRender	=	false;
 	}
-	
+
 	function Delete($ID=NULL)
 	{
 		if($this->access[$this->aco_id]["_delete"] != "1")
@@ -339,13 +339,13 @@ class ArosController extends AppController
 			$this->autoRender	=	false;
 			return;
 		}
-		
+
 		$detail = $this->{$this->ModelName}->find('first', array(
 			'conditions' => array(
 				"{$this->ModelName}.id"		=>	$ID
 			)
 		));
-		
+
 		if(empty($detail))
 		{
 			$message	=	"Item not found.";
@@ -359,11 +359,11 @@ class ArosController extends AppController
 			$this->{$this->ModelName}->delete($ID,false);
 			$message	=	"Data has deleted.";
 		}
-		
+
 		echo json_encode(array("data"=>array("message"=>$message)));
 		$this->autoRender	=	false;
 	}
-	
+
 	function SuccessAdd($ID=NULL)
 	{
 		$data = $this->{$this->ModelName}->find('first', array(
@@ -378,7 +378,7 @@ class ArosController extends AppController
 		}
 		$this->set(compact("ID"));
 	}
-	
+
 	function SuccessEdit($ID=NULL,$page=1,$viewpage=50)
 	{
 		$data = $this->{$this->ModelName}->find('first', array(
@@ -386,7 +386,7 @@ class ArosController extends AppController
 				"{$this->ModelName}.id" 		=> $ID
 			)
 		));
-		
+
 		if(empty($data))
 		{
 			$this->layout	=	"ajax";
